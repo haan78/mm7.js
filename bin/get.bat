f1fb7ff7521de9d2c3d0dbb00681c232b3ff3215
BARIS <BARIS@BARIS-PC>
2017-08-08 10:37:08 +0300

    Version 2.0 content modulu eklendi http guncellendi

--- bin/get.bat
+++ bin/get.bat
@@ -1,4 +1,33 @@
-wget -q --no-check-certificate https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.base.js
-wget -q --no-check-certificate https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.base.min.js
-wget -q --no-check-certificate https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.all.in.one.js
-wget -q --no-check-certificate https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.all.in.one.min.js
+@echo off
+
+if [%1] equ [] goto help
+
+
+if exist %1\mm7.standart.js (
+    del %1\mm7.standart.js
+)
+
+if exist %1\mm7.standart.min.js (
+    del %1\mm7.standart.min.js
+)
+
+if exist %1\mm7.extended.js (
+    del %1\mm7.extended.js
+)
+
+if exist %1\mm7.extended.min.js (
+    del %1\mm7.extended.min.js
+)
+
+
+
+wget -q --no-check-certificate -P %1 https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.standart.js
+wget -q --no-check-certificate -P %1 https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.standart.min.js
+wget -q --no-check-certificate -P %1 https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.extended.js
+wget -q --no-check-certificate -P %1 https://raw.githubusercontent.com/haan78/mm7.js/master/dist/mm7.extended.min.js
+
+goto :eof
+
+:help
+echo mm7.js get help
+echo get [folder]
