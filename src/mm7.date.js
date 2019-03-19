254a7630ad763a794ef6c0d680a9292a71a488be
BARIS <BARIS@BARIS-PC>
2019-03-19 10:16:41 +0200

    Bu ilk commit

--- src/mm7.date.js
+++ src/mm7.date.js
@@ -14,42 +14,76 @@
  * %D Day of Week (Only for toString)
  */
 
-(function(mm7){
+(function (mm7) {
     mm7["date"] = {
-    //daysOfWeek:["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"],
-    daysOfWeek:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-    //monthsOfYear:["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Tenmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"],
-    monthsOfYear:["January","February","March","April","May","June","July","August","September","October","November","December"],
-    getDefaultElements: function() {
-        var obj = new Object();
-        obj["year"] = 0;
-        obj["month"] = 0;
-        obj["day"] = 0;
-        obj["hour"] = 0;
-        obj["minute"] = 0;
-        obj["second"] = 0;
-        obj["millisecond"] = 0;
-        obj["dayOfWeek"] = 0;
-        return obj;
-    },
-    getElementsFromDate: function(d) {
-        var obj = new Object();
-        obj["year"] = d.getFullYear();
-        obj["month"] = d.getMonth() + 1;
-        //obj["day"] = d.getUTCDate();
-        obj["day"] = d.getDate();
-        obj["hour"] = d.getHours();
-        obj["minute"] = d.getMinutes();
-        obj["second"] = d.getSeconds();
-        obj["millisecond"] = d.getMilliseconds();
-        obj["dayOfWeek"] = d.getDay();
-        obj["dayName"] = this.daysOfWeek[d.getDay()];
-        obj["monthName"] = this.monthsOfYear[d.getMonth()];
-        return obj;
-    },
-    
-    diff:function(d1,d2,interval) { //interval = (second,minute,day,week,mounth,year)
-        var result = d2 - d1;
+        //daysOfWeek:["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"],
+        daysOfWeek: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        //monthsOfYear:["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Tenmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"],
+        monthsOfYear: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        getDefaultElements: function () {
+            var obj = new Object();
+            obj["year"] = 0;
+            obj["month"] = 0;
+            obj["day"] = 0;
+            obj["hour"] = 0;
+            obj["minute"] = 0;
+            obj["second"] = 0;
+            obj["millisecond"] = 0;
+            obj["dayOfWeek"] = 0;
+            return obj;
+        },
+        getElementsFromDate: function (d) {
+            var obj = new Object();
+            obj["year"] = d.getFullYear();
+            obj["month"] = d.getMonth() + 1;
+            //obj["day"] = d.getUTCDate();
+            obj["day"] = d.getDate();
+            obj["hour"] = d.getHours();
+            obj["minute"] = d.getMinutes();
+            obj["second"] = d.getSeconds();
+            obj["millisecond"] = d.getMilliseconds();
+            obj["dayOfWeek"] = d.getDay();
+            obj["dayName"] = this.daysOfWeek[d.getDay()];
+            obj["monthName"] = this.monthsOfYear[d.getMonth()];
+            return obj;
+        },
+
+        add: function (d, interval, num) {
+            var dr = new Date(d.getTime());
+            switch (interval) {
+                case "year":
+                    dr.setFullYear(d.getFullYear() + num);
+                    break;
+                case "mounth":
+                    dr.setMonth(d.getMonth() + num);
+                    break;
+                case "week":
+                    dr.setDate(d.getDate() + (num * 7));
+                    break;
+                case "day":
+                    dr.setDate(d.getDate() + num);
+                    break;
+                case "hour":
+                    dr.setHours(d.getHours() + num);
+                    break;
+                case "minute":
+                    dr.setMinutes(d.getMinutes() + num);
+                    break;
+                case "second":
+                    dr.setSeconds(d.getSeconds() + num);
+                    break;
+                case "millisecond":
+                    dr.setMilliseconds(d.getMilliseconds() + num);
+                    break;
+                default:
+                    dr.setMilliseconds(d.getMilliseconds() + num);
+                    break;
+            }
+            return dr;
+        },
+
+        diff: function (d1, d2, interval) { //interval = (second,minute,day,week,mounth,year,millisecond)
+            var result = d2 - d1;
             switch (interval) {
                 case "year":
                     var ynew = d2.getFullYear();
@@ -59,10 +93,12 @@
                     var mold = d1.getMonth();
                     var dold = d1.getDate();
                     var diff = ynew - yold;
-                    if (mold > mnew) diff--;
+                    if (mold > mnew)
+                        diff--;
                     else {
                         if (mold == mnew) {
-                            if (dold > dnew) diff--;
+                            if (dold > dnew)
+                                diff--;
                         }
                     }
                     return diff;
@@ -88,190 +124,176 @@
                 case "second":
                     return result / (1000);
                     break;
+                case "millisecond":
+                    return result; // millisecond
+                    break;
                 default:
-                    return result; //
-                    break;;
-            }
-        
-    },
-    isNumberCode: function(code) {
-        if ((code > 47) && (code < 58))
-            return true;
-        else
-            return false;
-    },
-    isParamCode: function(code) {
-        if (
-                ((code > 47) && (code < 58)) || //0-9
-                ((code > 64) && (code < 91)) || // A-Z
-                ((code > 96) && (code < 123)) //a-z
-                )
-            return true;
-        else
-            return false;
-    },
-    getNumberValue: function(value) {
-        var num = parseInt(value);
-        if (isNaN(num))
-            num = -1;
-        else
-            return num;
-    },
-    getNumberPart: function(str) {
-        var s = "";
-        for (var i = 0; i < str.length; i++) {
-            if (this.isNumberCode(str.charCodeAt(i)))
-                s += str.charAt(i);
-            else
-                return s;
-        }
-        return s;
-    },
-    getParamMap: function(format) {
-        var map = new Array();
-        var s = "";
-        var last = 0;
-        var flag = false;
-        for (var i = 0; i < format.length; i++) {
-            if (flag) {
-                if (this.isParamCode(format.charCodeAt(i))) {
-                    s += format.charAt(i);
-                    if (i === format.length - 1) {
-                        map.push({param: s, index: last});
-                        s = "";
-                        break;
-                    }
-                } else {
-                    map.push({param: s, index: last});
-                    s = "";
-                    flag = false;
-                }
+                    return result; // millisecond
+                    break;
             }
 
-            if (format.charAt(i) === "%") {
-                flag = true;
-                last = i;
+        },
+        isNumberCode: function (code) {
+            if ((code > 47) && (code < 58))
+                return true;
+            else
+                return false;
+        },
+        isParamCode: function (code) {
+            if (
+                    ((code > 47) && (code < 58)) || //0-9
+                    ((code > 64) && (code < 91)) || // A-Z
+                    ((code > 96) && (code < 123)) //a-z
+                    )
+                return true;
+            else
+                return false;
+        },
+        getNumberValue: function (value) {
+            var num = parseInt(value);
+            if (isNaN(num))
+                num = -1;
+            else
+                return num;
+        },
+        getNumberPart: function (str) {
+            var s = "";
+            for (var i = 0; i < str.length; i++) {
+                if (this.isNumberCode(str.charCodeAt(i)))
+                    s += str.charAt(i);
+                else
+                    return s;
             }
+            return s;
+        },
+        getParamMap: function (format) {
+            var map = new Array();
+            var s = "";
+            var last = 0;
+            var flag = false;
+            for (var i = 0; i < format.length; i++) {
+                if (flag) {
+                    if (this.isParamCode(format.charCodeAt(i))) {
+                        s += format.charAt(i);
+                        if (i === format.length - 1) {
+                            map.push({param: s, index: last});
+                            s = "";
+                            break;
+                        }
+                    } else {
+                        map.push({param: s, index: last});
+                        s = "";
+                        flag = false;
+                    }
+                }
+
+                if (format.charAt(i) === "%") {
+                    flag = true;
+                    last = i;
+                }
+            }
+            return map;
+        },
+        daysInMonth: function (date) {
+            return new Date(date.getYear(), date.getMonth() + 1, 0).getDate();
+        },
+        toDate: function (str, format) {
+            var map = this.getParamMap(format);
+            var elements = this.getDefaultElements();
+            var j = 0;
+            var p = 0;
+            for (var i = 0; i < map.length; i++) {
+                j = p + map[i].index;
+                var v = "";
+                if ((map[i].param === "d") || (map[i].param === "dd")) {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.day = this.getNumberValue(v);
+                } else if ((map[i].param === "M") || (map[i].param === "MM")) {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.month = this.getNumberValue(v);
+                } else if (map[i].param === "Y") {
+                    v = this.getNumberPart(str.substr(j, 4));
+                    elements.year = this.getNumberValue(v);
+                } else if (map[i].param === "yy") {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.year = this.getNumberValue(v) + 2000;
+                } else if ((map[i].param === "H") || (map[i].param === "HH")) {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.hour = this.getNumberValue(v);
+                } else if ((map[i].param === "m") || (map[i].param === "mm")) {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.minute = this.getNumberValue(v);
+                } else if ((map[i].param === "s") || (map[i].param === "ss")) {
+                    v = this.getNumberPart(str.substr(j, 2));
+                    elements.second = this.getNumberValue(v);
+                } else if (map[i].param === "ms") {
+                    v = this.getNumberPart(str.substr(j, 3));
+                    elements.millisecond = this.getNumberValue(v);
+                }
+                p = p + v.length - (map[i].param.length + 1);
+            }
+            return new Date(elements["year"], elements["month"] - 1, elements["day"], elements["hour"], elements["minute"], elements["second"], elements["millisecond"]);
+        },
+        fillZero: function (num) {
+            if (num < 10)
+                return "0" + num;
+            else
+                return "" + num;
+        },
+        toString: function (date, format) {
+            var elements = this.getElementsFromDate(date);
+            var map = this.getParamMap(format);
+            var str = format;
+            for (var i = 0; i < map.length; i++) {
+                if (map[i].param === "D") {
+                    str = str.replace("%D", elements.dayName);
+                } else
+                if (map[i].param === "Mo") {
+                    str = str.replace("%Mo", elements.monthName);
+                } else
+                if (map[i].param === "d")
+                    str = str.replace("%d", elements.day);
+                else
+                if (map[i].param === "dd")
+                    str = str.replace("%dd", this.fillZero(elements.day));
+                else
+                if (map[i].param === "M")
+                    str = str.replace("%M", elements.month);
+                else
+                if (map[i].param === "MM")
+                    str = str.replace("%MM", this.fillZero(elements.month));
+                else
+                if (map[i].param === "Y")
+                    str = str.replace("%Y", elements.year);
+                else
+                if (map[i].param === "yy")
+                    str = str.replace("%yy", elements.year % 100);
+                else
+                if (map[i].param === "H")
+                    str = str.replace("%H", elements.hour);
+                else
+                if (map[i].param === "HH")
+                    str = str.replace("%HH", this.fillZero(elements.hour));
+                else
+                if (map[i].param === "m")
+                    str = str.replace("%m", elements.minute);
+                else
+                if (map[i].param === "mm")
+                    str = str.replace("%mm", this.fillZero(elements.minute));
+                else
+                if (map[i].param === "s")
+                    str = str.replace("%s", elements.second);
+                else
+                if (map[i].param === "ss")
+                    str = str.replace("%ss", this.fillZero(elements.second));
+                else
+                if (map[i].param === "ms")
+                    str = str.replace("%ms", elements.millisecond);
+            }
+            return str;
+        },
+        convert: function (sDate, format1, format2) {
+            return mm7.date.toString(mm7.date.toDate(sDate, format1), format2);
         }
-        return map;
-    },
-    daysInMonth:function(date) {
-        return new Date(date.getYear(),date.getMonth()+1,0).getDate();
-    },
-    add:function(date,unit,num) {
-        var d = new Date(date);
-        if (unit==="day") {
-            d.setDate( d.getDate() + num );
-        } else if ( unit === "month" ) {
-            d.setMonth( d.getMonth() + num );
-        } else if ( unit === "year" ) {
-            d.setYear( d.getYear() + num );
-        } else if ( unit === "hour" ) {            
-            d.setHours( d.getHours() + num );
-        } else if ( unit === "minute" ) {            
-            d.setMinutes( d.getMinutes() + num );
-        } else if ( unit === "second" ) {
-            d.setSeconds( d.getSeconds() + num );
-        }
-        return d;
-    },
-    toDate: function(str, format) {           
-        var map = this.getParamMap(format);
-        var elements = this.getDefaultElements();
-        var j = 0;
-        var p = 0;
-        for (var i = 0; i < map.length; i++) {
-            j = p + map[i].index;
-            var v = "";
-            if ((map[i].param === "d") || (map[i].param === "dd")) {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.day = this.getNumberValue(v);
-            } else if ((map[i].param === "M") || (map[i].param === "MM")) {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.month = this.getNumberValue(v);
-            } else if (map[i].param === "Y") {
-                v = this.getNumberPart(str.substr(j, 4));
-                elements.year = this.getNumberValue(v);
-            } else if (map[i].param === "yy") {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.year = this.getNumberValue(v) + 2000;
-            } else if ((map[i].param === "H") || (map[i].param === "HH")) {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.hour = this.getNumberValue(v);
-            } else if ((map[i].param === "m") || (map[i].param === "mm")) {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.minute = this.getNumberValue(v);
-            } else if ((map[i].param === "s") || (map[i].param === "ss")) {
-                v = this.getNumberPart(str.substr(j, 2));
-                elements.second = this.getNumberValue(v);
-            } else if (map[i].param === "ms") {
-                v = this.getNumberPart(str.substr(j, 3));
-                elements.millisecond = this.getNumberValue(v);
-            }            
-            p = p + v.length - (map[i].param.length + 1);            
-        }
-        return new Date(elements["year"], elements["month"] - 1, elements["day"], elements["hour"], elements["minute"], elements["second"], elements["millisecond"]);
-    },
-    fillZero: function(num) {
-        if (num < 10)
-            return "0" + num;
-        else
-            return "" + num;
-    },
-    toString: function(date, format) {
-        var elements = this.getElementsFromDate(date);
-        var map = this.getParamMap(format);
-        var str = format;
-        for (var i = 0; i < map.length; i++) {
-            if (map[i].param === "D") {
-                str = str.replace("%D", elements.dayName);
-            } else
-            if (map[i].param === "Mo") {
-                str = str.replace("%Mo",elements.monthName);
-            } else
-            if (map[i].param === "d")
-                str = str.replace("%d", elements.day);
-            else
-            if (map[i].param === "dd")
-                str = str.replace("%dd", this.fillZero(elements.day));
-            else
-            if (map[i].param === "M")
-                str = str.replace("%M", elements.month);
-            else
-            if (map[i].param === "MM")
-                str = str.replace("%MM", this.fillZero(elements.month));
-            else
-            if (map[i].param === "Y")
-                str = str.replace("%Y", elements.year);
-            else
-            if (map[i].param === "yy")
-                str = str.replace("%yy", elements.year % 100);
-            else
-            if (map[i].param === "H")
-                str = str.replace("%H", elements.hour);
-            else
-            if (map[i].param === "HH")
-                str = str.replace("%HH", this.fillZero(elements.hour));
-            else
-            if (map[i].param === "m")
-                str = str.replace("%m", elements.minute);
-            else
-            if (map[i].param === "mm")
-                str = str.replace("%mm", this.fillZero(elements.minute));
-            else
-            if (map[i].param === "s")
-                str = str.replace("%s", elements.second);
-            else
-            if (map[i].param === "ss")
-                str = str.replace("%ss", this.fillZero(elements.second));
-            else
-            if (map[i].param === "ms")
-                str = str.replace("%ms", elements.millisecond);
-        }
-        return str;
-    },
-    convert:function(sDate,format1,format2) {
-        return mm7.date.toString(mm7.date.toDate(sDate,format1),format2);
-    }
-};
+    };
 })(mm7);
